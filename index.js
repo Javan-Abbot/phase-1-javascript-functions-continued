4841a866bdc4c09f4daab71b118e80513b464cec
+javan abbot <+>
2024-06-30 09:07:20 +0300

    functions ctnd

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+console.log(saturdayFun("bathe my dog"));
+console.log(saturdayFun());
+
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+console.log(mondayWork());
+console.log(mondayWork("work from home"));
+
+function wrapAdjective(adjective="*"){
+        return function(symbol="a hard worker"){
+            return `You are ${adjective}${symbol}${adjective}!`;
+    }
+}
+console.log(wrapAdjective()());
+console.log(wrapAdjective("a dedicated programmer")("||"));
+
+
